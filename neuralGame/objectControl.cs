4538cc6e3697cb4913a1a04a60a22b5c2d445c8a
Ali Rassolie <ali.rassolie@gmail.com>
2018-06-09 17:34:56 +0200

    Create objectControl.cs

---
+++ neuralGame/objectControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+    public static GameControl instance = null;
+
+    public static bool gameStopped = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
